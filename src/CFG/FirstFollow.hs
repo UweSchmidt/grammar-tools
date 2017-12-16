5783f1060c5e769e1d958abba206f0ea67151a85
Uwe Schmidt <uwe@fh-wedel.de>
2017-12-16 18:16:40 +0100

    FirstFollow refactored

--- src/CFG/FirstFollow.hs
+++ src/CFG/FirstFollow.hs
@@ -5,7 +5,7 @@ import qualified Prelude as P
 
 import           Control.Arrow ((&&&))
 
-import           Data.Set  (empty, foldl', insert, member, singleton, union)
+import           Data.Set  (Set, empty, foldl', insert, member, singleton, union)
 import           Data.List (tails)
 
 import           CFG.Types
@@ -41,27 +41,17 @@ intermediates (x1 : xs1@(x2 : _))
 
 -- ----------------------------------------
 --
--- the loop over all rules in a grammar
+-- loop over a set of values, e.g. Symbols, Rules, ...
 
-forEachRule :: (Rule -> a -> a) -> Rules -> a -> a
-forEachRule processRule rs acc =
-  foldl' (flip processRule) acc rs
+forEachElem :: (v -> a -> a) -> Set v -> a -> a
+forEachElem op =
+  flip (foldl' (flip op))
 
--- loop over all symbols in a word, e.g. a RHS of rule
-
-forEachSymbol :: (Symbol -> a -> a) -> Word -> a -> a
-forEachSymbol = forEach
-
-forEachWord :: (Word -> a -> a) -> [Word] -> a -> a
-forEachWord = forEach
+-- loop over a list of values, e.g. Symbols, Words, ...
 
 forEach :: (v -> a -> a) -> [v] -> a -> a
-forEach op vs acc =
-  P.foldr op acc vs
-
-forEachSym :: (Symbol -> a -> a) -> SymSet -> a -> a
-forEachSym processSymbol syms acc =
-  foldl' (flip processSymbol) acc syms
+forEach op =
+  flip (P.foldr op)
 
 -- ----------------------------------------
 --
@@ -79,12 +69,14 @@ nullables' (n, t, rules, s) =
   where
     nullSyms :: SymSet -> SymSet
     nullSyms nsys =
-      forEachRule nullSym rules nsys
+      forEachElem nullSym rules nsys
       where
         nullSym :: Rule -> SymSet -> SymSet
         nullSym (x, ys) acc
+
           -- nullable right hand side found
           -- insert left hand side into the result
+
           | nullableWord nsys ys = insert x acc
           | otherwise            =          acc
 
@@ -92,8 +84,8 @@ nullables' (n, t, rules, s) =
 -- test nullable for all symbols in a word
 
 nullableWord :: SymSet -> Word -> Bool
-nullableWord nullables w =
-  forEachSymbol (\ y res -> y `member` nullables && res) w True
+nullableWord nulls w =
+  forEach (\ y r -> y `member` nulls && r) w True
 
 -- ----------------------------------------
 --
@@ -102,23 +94,23 @@ nullableWord nullables w =
 -- get the least fixpoint of the list of first sets
 
 firstSets :: SymSet -> Grammar -> SymMap
-firstSets nullables =
-  fixpoint . firstSets' nullables
+firstSets nulls =
+  fixpoint . firstSets' nulls
 
 -- for traces we want the list of all intermediate results
 
 firstSets' :: SymSet -> Grammar -> [SymMap]
-firstSets' nullables (n, t, rules, s) =
+firstSets' nulls (n, t, rules, s) =
   iterate firstSyms initFirstSyms
   where
     firstSyms :: SymMap -> SymMap
     firstSyms fsyms =
-      forEachRule firstSym rules fsyms
+      forEachElem firstSym rules fsyms
       where
         -- insert first set of RHS into firstSyms of LHS
         firstSym :: Rule -> SymMap -> SymMap
         firstSym (x, ys) acc =
-          insertSyms x (first nullables fsyms ys) acc
+          insertSyms x (first nulls fsyms ys) acc
 
     -- init first map
     -- for all terminal syms t: first(t) = {t}
@@ -128,53 +120,43 @@ firstSets' nullables (n, t, rules, s) =
       where
         initT, initN :: SymMap
         initT =
-          forEachSym (\sym -> insertSyms sym (singleton sym)) t emptySyms
+          forEachElem (\sym -> insertSyms sym (singleton sym)) t emptySyms
 
         initN =
-          forEachSym (\sym -> insertSyms sym empty) n emptySyms
+          forEachElem (\sym -> insertSyms sym empty) n emptySyms
 
 -- take a word [y1,y2,..,yn], e.g. a right hand side
 -- of a grammar rule, and compute the FIRST set
--- with respect to the table "fsets" and the "nullables" set
+-- with respect to the table "fsets" and the "nulls" set
 
 first :: SymSet -> SymMap -> Word -> SymSet
-first nullables fSets w =
-  forEachSymbol firstSym w empty
+first nulls fSets w =
+  forEach firstSym w empty
   where
     firstSym :: Symbol -> SymSet -> SymSet
     firstSym x r
-      | x `member` nullables = fx `union` r
+      | x `member` nulls = fx `union` r
       | otherwise            = fx
       where
         fx = lookupSyms x fSets
-  {-
-  go empty w
-  where
-    go acc []               = acc
-    go acc (y1 : ys)
-      | y1 `member` nullables = go acc1 ys
-      | otherwise           =    acc1
-      where
-        -- add first(y1) to result
-        acc1 = lookupSyms y1 fSets `union` acc
--}
+
 -- ----------------------------------------
 --
 -- the computation of the follow sets
 
 followSets :: SymSet -> SymMap -> Grammar -> SymMap
-followSets nullables firsts g =
-  fixpoint $ followSets' nullables firsts g
+followSets nulls firsts g =
+  fixpoint $ followSets' nulls firsts g
 
 -- for traces we want the list of intermediate results
 
 followSets' :: SymSet -> SymMap -> Grammar -> [SymMap]
-followSets' nullables firsts (n, t, rules, s) =
+followSets' nulls firsts (n, t, rules, s) =
   iterate followSyms initFollowSyms
   where
     followSyms :: SymMap -> SymMap
     followSyms fsyms =
-      forEachRule followSym rules fsyms
+      forEachElem followSym rules fsyms
       where
         followSym :: Rule -> SymMap -> SymMap
         followSym (x, ys) = followX (reverse ys) . followYS ys
@@ -187,26 +169,18 @@ followSets' nullables firsts (n, t, rules, s) =
 
             followX :: Word -> SymMap -> SymMap
             followX ys' sm =
-              forEachSymbol addFX ys' emptySyms `unionSyms` sm
+              forEach addFX ys' emptySyms `unionSyms` sm
               where
                 addFX :: Symbol -> SymMap -> SymMap
                 addFX y r
-                  | y `member` nullables = r' `unionSyms` r
+                  | y `member` nulls = r' `unionSyms` r
                   | otherwise            = r'
                   where
                     r' = singletonSyms y (lookupSyms x fsyms)
 
-{-
-            followX []         acc  = acc
-            followX (yn : ys') acc
-              | yn `member` nullables = followX ys' acc1
-              | otherwise           =             acc1
-              where
-                acc1 = insertSyms yn (lookupSyms x fsyms) acc
--}
-
             -- extend follow(y1) by first(y2) and
             -- in case of nullable(y2) the y3 and so on
+
             followYS :: Word -> SymMap -> SymMap
             followYS w sm =
               forEach followRHS (rhs w) sm
@@ -220,30 +194,11 @@ followSets' nullables firsts (n, t, rules, s) =
 
                 followRHS :: (Symbol, Word) -> SymMap -> SymMap
                 followRHS (y1, ys) r1 =
-                  forEach followY ys emptySyms `unionSyms` r1
-                  where
-                    followY :: Symbol -> SymMap -> SymMap
-                    followY y2 r
-                      | y2 `member` nullables = r' `unionSyms` r
-                      | otherwise             = r'
-                      where
-                        r' = singletonSyms y1 (lookupSyms y2 firsts)
+                  insertSyms y1 (first nulls firsts ys) r1
 
-
-{-
-            followYS []         acc0 = acc0
-            followYS (y1 : ys') acc0 = followYS ys' . followY1 ys' $ acc0
-              where
-                followY1 []          acc' = acc'
-                followY1 (y2 : ys'') acc'
-                  | y2 `member` nullables   = followY1 ys'' acc1'
-                  | otherwise             =               acc1'
-                  where
-                    acc1' = insertSyms y1 (lookupSyms y2 firsts) acc'
--}
     initFollowSyms :: SymMap
     initFollowSyms =
-      forEachSym (\sym -> insertSyms sym empty) (n `union` t) emptySyms
+      forEachElem (\sym -> insertSyms sym empty) (n `union` t) emptySyms
 
 -- ----------------------------------------
 
@@ -251,9 +206,21 @@ nullsFirstsFollows :: Grammar -> (SymSet, SymMap, SymMap)
 nullsFirstsFollows g@(n, t, rules, s) =
   (nulls, firsts, follows)
   where
-    nulls  = nullables g
-    firsts  = firstSets nulls g
+    nulls   = nullables  g
+    firsts  = firstSets  nulls g
     follows = followSets nulls firsts g
+
+-- ----------------------------------------
+--
+-- first and follow sets with trace of iterations
+
+nullsFirstsFollows' :: Grammar -> ([SymSet], [SymMap], [SymMap])
+nullsFirstsFollows' g@(n, t, rules, s) =
+  (nulls', firsts', follows')
+  where
+    nulls'   = intermediates $ nullables'  g
+    firsts'  = intermediates $ firstSets'  (last nulls') g
+    follows' = intermediates $ followSets' (last nulls') (last firsts') g
 
 -- ----------------------------------------
 
